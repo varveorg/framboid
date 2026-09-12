c67d81651c70e05ef60c4f15aec922d8ee0e5e7a
ZA1815 <zaahme18@gmail.com>
2026-09-11 21:46:35 -0400

    refactor(core/account/dossier/event): Strip down event, will construct again after stream is done

--- src/account/dossier/event.rs
+++ src/account/dossier/event.rs
@@ -1,18 +1,6 @@
-use std::collections::HashSet;
-
 use time::Timestamp;
 
 pub struct Event {
-    kind: EventKind,
-    // Decide type soon
-    instruments: HashSet<()>,
-    // Decide type soon
-    mediums: HashSet<()>,
     from: Timestamp,
     to: Timestamp
-}
-
-pub enum EventKind {
-    Natural,
-    Synthetic
 }
