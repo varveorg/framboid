43da56c1a9d042cf56fb26c4fff5e9a0cfe78443
ZA1815 <zaahme18@gmail.com>
2026-09-14 18:07:30 -0400

    feat(core/account/dossier/event): Add the Actions map to the Event structure

--- src/account/dossier/event.rs
+++ src/account/dossier/event.rs
@@ -1,7 +1,12 @@
+use blake3::Hash;
+use indexmap::IndexMap;
 use time::Timestamp;
+
+use crate::addressing::Action;
 
 /// A slice of time deemed meaningful.
 pub struct Event {
+    actions: IndexMap<Hash, Action>,
     from: Timestamp,
     to: Timestamp
 }
